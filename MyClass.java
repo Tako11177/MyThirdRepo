8fbe39b9b2d30af1c2049285097870c055e80061
Tako11177 <takokorinteli7@gmail.com>
2025-05-02 13:32:35 +0400

    1122w

---
+++ MyClass.java
@@ -0,0 +1,17 @@
+public class MyClass {
+
+    public static void main(String[] args) {
+
+        int age = 35;
+        boolean isStudent = true;
+
+        if (age >=18 || age <=30) {
+            System.out.println("ჯენზი");
+        } else {
+            System.out.println("სხვა");
+
+
+        }
+
+    }
+}
